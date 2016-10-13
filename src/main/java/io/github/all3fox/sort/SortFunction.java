db28179c46642501419e7f37e2c4f8db0bec0b1f
Alexander Lisianoi <all3fox@gmail.com>
2016-10-13 15:04:45 +0200

    Use TestNG as testing framework, remove SortFunction interface
    Interface disallows to have static sort method

--- src/main/java/io/github/all3fox/sort/SortFunction.java
+++
@@ -1,9 +0,0 @@
-package io.github.all3fox.sort;
-
-import java.util.Comparator;
-import java.util.List;
-
-public interface SortFunction {
-    <T extends Comparable<? super T>> void sort(List<T> xs);
-    <T> void sort(List<T> xs, Comparator<? super T> cmp);
-}
